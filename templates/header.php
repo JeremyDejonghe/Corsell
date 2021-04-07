1f44b11431df4fe137caea496aa1f00d544ae3c0
Jérémy Dejonghe <jeremy.dejonghe59@gmail.com>
2021-04-07 17:04:08 +0200

    Réalisation et début de la stylisation

--- templates/header.php
+++ templates/header.php
@@ -13,5 +13,66 @@
 
 <body>
     <header>
-        <h1>COR-SELL</h1>
+        <div class="logo">
+            <img src="assets/img/Cor-Sell_Logo.png" alt="">
+        </div>
+        <div class="nav1">
+
+            <div class="title">
+                <h1>Cor-Sell</h1>
+            </div>
+            <div class="search">
+                <input type="text">
+            </div>
+            <div class="bonjour">
+                <div>
+                    <p>Bonjour, Identifiez-vous</p>
+                </div>
+                <div>
+                    <p>Compte</p>
+                </div>
+            </div>
+            <div class="command">
+                <div>
+                    <p>Vos</p>
+                </div>
+                <div>
+                    <p>Commandes</p>
+                </div>
+            </div>
+            <div class="panier">
+                <figure>
+                    <img src="" alt="">
+                </figure>
+                <div>
+                    <p>Coffre</p>
+                </div>
+            </div>
+        </div>
+        <div class="nav2">
+            <a href="">
+                <p>Meilleures Ventes</p>
+            </a>
+            <a href="">
+                <p>Cor-sell Basics</p>
+            </a>
+            <a href="">
+                <p>Dernières Nouveautées</p>
+            </a>
+            <a href="">
+                <p>Navires</p>
+            </a>
+            <a href="">
+                <p>Victuailles</p>
+            </a>
+            <a href="">
+                <p>Armes</p>
+            </a>
+            <a href="">
+                <p>Matières premières</p>
+            </a>
+            <a href="">
+                <p>Nos Marques</p>
+            </a>
+        </div>
     </header>
