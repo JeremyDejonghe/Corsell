4464e710cf7873f43602ad022606d07d34d743d2
Jérémy Dejonghe <jeremy.dejonghe59@gmail.com>
2021-04-07 16:38:05 +0200

    Réalisation et stylisation du footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,2 +1,23 @@
+<footer>
+    <div class="footer-block">
+        <p class="-title">Pour mieux nous connaître</p>
+        <a href=""><p>À propos de Cor-sell</p></a>
+    </div>
+    <div class="footer-block">
+        <p class="-title">Moyen de paiement Cor-sell</p>
+        <a href=""><p>Or</p></a>
+        <a href=""><p>Lettres de changes</p></a>
+        <a href=""><p>Troc</p></a>
+        <a href=""><p>Rechargez votre compte</p></a>
+    </div>
+    <div class="footer-block">
+        <p class="-title">Besoin d'aide ?</p>
+        <a href=""><p>Service Client</p></a>
+        <a href=""><p>Accessibilité</p></a>
+        <a href=""><p>Cor-sell P</p></a>
+    </div>
+</footer>
+
 </body>
+
 </html>
