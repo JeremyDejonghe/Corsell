87d5c5a6c1af9695aee69f6126910e3e38ca5148
Jérémy Dejonghe <jeremy.dejonghe59@gmail.com>
2021-04-08 14:12:28 +0200

    Structuration de HomeMVC + appels dynamiques

--- src/controller/home.php
+++ src/controller/home.php
@@ -11,9 +11,27 @@ class HomeController
     /**
      * Récupération des produits en bdd
      */
-    // public function getProducts() {
-    //     $res = $this->model->db->query("");
+    public function getMostSell() {
+        $res = $this->model->db->query("SELECT id, picture FROM products ORDER BY sell_number DESC LIMIT 0,4");
         
-    //     return $res;
-    // }
+        return $res;
+    }
+
+    public function getLastProducts() {
+        $res = $this->model->db->query("SELECT id FROM products ORDER BY id DESC LIMIT 0,4");
+
+        return $res;
+    }
+
+    public function getMostSellWeapons() {
+        $res = $this->model->db->query("SELECT id, picture FROM products WHERE id_category = 1 ORDER BY sell_number DESC LIMIT 0,5");
+
+        return $res;
+    }
+
+    public function getCategory() {
+        $res = $this->model->db->query("SELECT id, name, picture FROM category");
+
+        return $res;
+    }
 }
--- templates/home.php
+++ templates/home.php
@@ -1,8 +1,56 @@
-<?php 
+<?php
 $home = true;
 require_once("header.php");
 ?>
+<main>
+    <section class="header-wrapper">
+        <figure>
+            <img src="assets/img/background-header.png" alt="">
+        </figure>
 
-<?php 
+        <div class="card">
+            <h2>Les plus vendus</h2>
+            <?php foreach ($mostSells as $mostSell) { ?>
+                <img src="assets/img/products/<?= $mostSell["picture"] ?>" alt="">
+            <?php } ?>
+        </div>
+        <div class="card">
+            <h2>Les essentiels du Pirate</h2>
+            <img src="assets/img/essentials.png" alt="">
+        </div>
+        <div class="card">
+            <h2>Dernières nouveautées</h2>
+            <?php foreach ($lastProducts as $lastProduct) { ?>
+                <img src="assets/img/products/<?= $lastProduct["picture"] ?>" alt="">
+            <?php } ?>
+        </div>
+    </section>
+
+    <div class="banner">
+        <img src="assets/img/banner_commercial.png" alt="">
+    </div>
+
+    <section class="category">
+        <?php foreach ($categories as $category) { ?>
+            <div class="card">
+                <h2><?= $category["name"] ?></h2>
+                <img src="assets/img/products/<?= $category["picture"] ?>" alt="">
+            </div>
+        <?php } ?>
+
+        <div class="card-line">
+            <h2>Meilleures ventes dans armes</h2>
+            <?php foreach ($mostSellWeapons as $mostSellWeapon) { ?>
+                <img src="assets/img/products/<?= $mostSellWeapon["picture"] ?>" alt="">
+            <?php } ?>
+        </div>
+    </section>
+
+    <div class="banner">
+        <img src="assets/img/banner_commercial.png" alt="">
+    </div>
+
+</main>
+<?php
 require_once("footer.php");
 ?>
