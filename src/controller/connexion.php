1df543b62fbd61fe84b308d4b179a5bc8a0f4836
Alexandre ROY <66955818+Axyor@users.noreply.github.com>
2021-04-16 09:44:41 +0200

    Mise en place du style de la page connecxion + Ajout de la fonctionnalité de connexion à une session utilisateur selon sa category v1

--- src/controller/connexion.php
+++ src/controller/connexion.php
@@ -8,9 +8,28 @@ class ConnexionController
         $this->model = $model;
     }
 
-    public function getUsers()
+    public function getUser($email)
     {
-        $res = $this->model->db->query("select * from users");
+        $res = $this->model->db->prepare("SELECT id, pseudo, email, avatar , id_users_category FROM users WHERE email = :email");
+        $res->bindValue(":email", $email, PDO::PARAM_STR);
+        $res->execute();
+        $res = $res->fetch();
         return $res;
+    }
+
+    
+    public function validateLogin(): bool
+    {
+        $result = $this->getUser($this->model->email);
+        if (!empty($result) && password_verify($this->model->password, $result["password"])) {
+            $_SESSION["session_id"] = md5($result["email"]);
+            $_SESSION["user_name"] = $result["firstname"];
+            $_SESSION["user_ip"] = $_SERVER["REMOTE_ADDR"];
+            $_SESSION["user_category"] =$result["id_users_category"];
+
+            return true;
+        }
+
+        return false;
     }
 }
--- templates/connexion.php
+++ templates/connexion.php
@@ -1,12 +1,31 @@
 <?php
 require_once("header-soft.php");
 ?>
+<main class="connexion-container">
 
+    <div class="element-block">
+        <h2>S'identifier</h2>
+        <?= $message ?>
+        <form action="" method="post">
+            <div class="form-group">
+                <label for="inputEmail">Adresse email</label>
+                <input type="email" name="email" id="inputEmail" >
+            </div>
 
+            <div class="form-group">
+            <label for="inputPassword">Mot de passe</label>
+                
+                <input type="password" name="password" id="inputPassword" >
+            </div>
+            <p>En passant votre commande, vous acceptez <span>les Conditions générales de vente</span> de Cor-sell.</p>
+            <p class="p-reset"><a href="">Mot de passe oublié ?</a></p>
+            <input class="btn-valid" type="submit" value="Se connecter">
+        </form>
+    </div>
 
+    <a class="registration-btn" href="./registration">Créer votre compte Cor-sell</a>
 
-
-
+</main>
 <?php
 require_once("footer.php");
 ?>
