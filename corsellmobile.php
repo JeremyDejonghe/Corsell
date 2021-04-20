0023e730f5af7daf94da40a513c35aa10e9dcf3e
Edouard Dhalluin <ed.dhalluin@gmail.com>
2021-04-20 17:54:35 +0200

    Réalisation du style de la page corsellMobile

--- corsellmobile.php
+++ corsellmobile.php
@@ -11,7 +11,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Spectral:ital,wght@0,400;0,600;0,800;1,400;1,600;1,800&display=swap" rel="stylesheet">
 </head>
 
-<body class="body-header-soft">
+<body class="container-corsellmobile">
 
     <div>
         <div class="main-corsellMobile">
@@ -32,8 +32,10 @@
             <div>
                 <p>Bientôt sur</p>
             </div>
-            <div>
+            <div class="svg-div-bottom-cossellMobile">
+                <div><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--grommet-icons" width="50" height="50" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="M3 17a1 1 0 0 1 0-2h10.5c1 0 2 2 1.5 2H3zm14 0a1 1 0 0 1 0-2h4a1 1 0 0 1 0 2h-4zM12.633 3.501a1 1 0 0 1 1.734.998L7.46 16.495a1 1 0 0 1-1.734-.997L12.633 3.5zM4 18.5c.5-1 3.5-2 2.5-.28A852.88 852.88 0 0 1 4.867 21a1 1 0 0 1-1.734-.998L4 18.5zM9.133 4.499a1 1 0 1 1 1.734-.998L12.61 6.53a1 1 0 1 1-1.733.998L9.133 4.499zM13 11.5c-.898-1.5 0-4.5.716-3.004L20.366 20a1 1 0 0 1-1.733.998L13 11.5z"></path></svg></div>
 
+                <div><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--uim" width="50" height="50" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path class="uim-tertiary" d="M14.556 12.895l2.573 2.554l3.785-2.186c.506-.253.83-.766.842-1.332a1.474 1.474 0 0 0-.837-1.275c-.315-.17-3.01-1.734-4.023-2.323l-.002-.002L4.639 1.256a1.644 1.644 0 0 0-1.657-.06c-.05.034-.097.071-.142.111l11.716 11.588z" opacity=".5" fill="currentColor"></path><path class="uim-secondary" d="M20.919 10.656c-.294-.159-2.657-1.53-3.791-2.188l-2.597 2.577L2.785 22.698c.048.044.1.084.153.12c.21.12.446.182.687.181c.292-.004.578-.084.83-.232c.389-.226 12.439-7.182 12.439-7.182l4.02-2.322c.506-.253.83-.766.842-1.332a1.474 1.474 0 0 0-.837-1.275z" opacity=".7" fill="currentColor"></path><path class="uim-quaternary" d="M13.61 11.96L2.84 1.309a1.509 1.509 0 0 0-.597 1.252v18.985c-.013.452.197.871.543 1.15L13.61 11.96z" opacity=".25" fill="currentColor"></path><path class="uim-primary" d="M20.919 10.656c-.744-.4-3.37-1.944-3.791-2.188l-2.597 2.577l-.921.914l.946.936l2.573 2.554l3.785-2.186c.506-.253.83-.766.842-1.332a1.474 1.474 0 0 0-.837-1.275z" fill="currentColor"></path></svg></div>
             </div>
         </div>
     </div>
