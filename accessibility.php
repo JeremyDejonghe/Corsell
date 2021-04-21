cf0a5ffb79d6de4efc1853cdcb9e2c2139efafc7
Edouard Dhalluin <ed.dhalluin@gmail.com>
2021-04-21 23:25:19 +0200

    Réalisation de la pague, des styles et du responsive de la page accessibility

---
+++ accessibility.php
@@ -0,0 +1,55 @@
+<?php
+session_start();
+require_once("templates/header.php");
+?>
+
+<div class="main-access">
+    <h1>Cor-sell Accessibilité</h1>
+    <div class="p-main-access">
+        <p> Nous sommes dans le regret d'informer notre aimable clientel que pour le moment notre site n'est pas à 100% accessible à tout le monde sans difficulté.
+        </p>
+        <p> Afin de nous excusé du désagrément et de vous faire profiter au mieux de notre site, notre équipe vous donne rendez-vous à la tavvenre du chat borgne.</p>
+        <p>Qu'importe votre handicape nos équipe seront vous aider.
+        </p>
+    </div>
+
+    <div class="p-main-access">
+        <h3>Handicap moteur?</h3>
+        <p>Joris notre intrepide pirate venant tout doit d angleterre est à votre disposition pour vous aider à trouver votre bonheur sur notre site.</p>
+        <div class="div-img-access">
+            <img src="./assets/img/james.png" alt="" class="img-access">
+        </div>
+    </div>
+   
+   
+
+    <div class="p-main-access">
+        <h3>Handicap visuel?</h3>
+        <p>Asma notre Artiste saura vous décrire avec minutie tout notre site.(même nos fautes de goûts)</p>
+        <div class="div-img-access">
+            <img src="./assets/img/asma.jpg" alt="" class="img-access">
+        </div>
+    </div>
+
+    <div class="p-main-access">
+        <h3>Handicap auditif?</h3>
+        <p>Piotr  proffesseur de langues des signe dans l'université de Tortuga pourra vous être d'une grande utilité.</p>
+        <div class="div-img-access">
+            <img src="./assets/img/piotr.jpg" alt="" class="img-access">
+        </div>
+    </div>
+
+    <div class="p-main-access">
+        <h3>Handicap mental?</h3>
+        <p>Laissez-vous guider par Dame Val'Issa pour pour baladersur notre site. En cas de stress son fildèle compagnon Lord Ronron sera la pour vous apaisé.</p>
+        <div class="div-img-access">
+            <img src="./assets/img/ValI.jpg" alt="" class="img-access">
+        </div>
+    </div>
+
+
+</div>
+
+<?php
+require_once("templates/footer.php");
+?>
