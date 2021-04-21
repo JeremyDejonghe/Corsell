6fb757e0b4b0070f999ff356800fc1d87d861b12
Edouard Dhalluin <ed.dhalluin@gmail.com>
2021-04-21 09:22:36 +0200

    Initialisation de la feature/apropos

---
+++ apropos.php
@@ -0,0 +1,89 @@
+<?php
+session_start();
+require_once("templates/header.php");
+?>
+
+<div class="main-apropos">
+    <div>
+        <h1>À Propos de Cor-sell</h1>
+    </div>
+    <h2>En apprendre plus sur le projet</h2>
+    <div>
+        <p>
+            Cor-sell est un projet de fin d'étude réaliser par Jérémy Dejonghe, Édouard D'Halluin et Édouard D'Halluin lors de leur formation chez M2I formation villeneuve d'ascq.Et voici son histoire:
+        </p>
+        <p class="Je-vais-sauter-quand-alex-me-verra">
+            Le jour où l'on annonca à leur classe qu'il y aurait un projet à faire en trinome nos trois comparses se mirrent d'accord pour le réaliser ensemble car même si il se connaissaient depuis peu ils étaient déjà inséparable comme peuvent l'être la baguette, le filet américain et les frites,chacun apportant leur qualité au projet. Très vite vient le moment de choisir le thème, Alexandre proposa de crée une marketplace du temps des pirates et c'est avec un grand enthousiassme que les deux autres acceptèrent. Ils se mirent tous au travail afin de peaufiner l'idée et ainsi être pret le jour J pour commencer à coder.
+        </p>
+        <p>
+            Les consignent étaient simple, réaliser un projet de a à z dans un temps impartie de 3 semaines, sans obligation sur les technos utilisés, sur le thème aborder.
+            En cas de blocage il était possible de demander de l 'aide à Frédéric Girod afin de nous aiguiller vers une solution.
+        </p>
+
+    </div>
+    <div >
+        <h2>En apprendre plus sur leur créateur</h2>
+
+        <div class="trinome-apropos">
+            <div>
+
+                <div>
+                    <h3>Jérémy Dejonghe</h3>
+                    <div>
+                        <div>
+                            <h4>En quelques mots</h4>
+                            <div>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta magnam aspernatur, odio nam nesciunt quod et eveniet asperiores repudiandae, cum assumenda atque iste reiciendis mollitia neque molestiae dolor voluptas deserunt.</p>
+                            </div>
+                            <div>
+                                <h4>quelques liens</h4>
+                                <a href="https://www.linkedin.com/in/j%C3%A9r%C3%A9my-dejonghe-6828a31b8/">Lien linkedin</a>
+                                <a href="https://jeremydejonghe.fr/#/">Site perso</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <h3>Édouard D'Halluin</h3>
+                    <div>
+                        <div>
+                            <h4>En quelques mots</h4>
+                            <div>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui atque accusamus praesentium, optio consectetur accusantium nihil molestiae saepe quas maiores ab error, aspernatur nulla doloribus voluptatum voluptatibus? Porro, labore dicta.</p>
+                            </div>
+                            <div>
+                                <h4>quelques liens</h4>
+                                <a href="https://www.linkedin.com/in/edouarddhalluin/">Lien linkedin</a>
+                                <a href="https://edouard-dhalluin.fr/#/">Site perso</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div>
+                    <h4>Alexandre Roy</h4>
+                    <div>
+                        <div>
+                            <h4>En quelques mots</h4>
+                            <div>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta magnam aspernatur, odio nam nesciunt quod et eveniet asperiores repudiandae, cum assumenda atque iste reiciendis mollitia neque molestiae dolor voluptas deserunt.</p>
+                            </div>
+                            <div>
+                                <h4>quelques liens</h4>
+                                <a href="https://www.linkedin.com/in/alexandre-roy-1086a96a/">Lien linkedin</a>
+                                <a href="https://alexandre-roy.web.app/">Site perso</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<?php
+require_once("templates/footer.php");
+?>
