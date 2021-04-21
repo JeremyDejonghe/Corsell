c07f5efebb3ead5eda32ca19dd1405843db763ff
Edouard Dhalluin <ed.dhalluin@gmail.com>
2021-04-21 22:32:52 +0200

    Réalition de la page needhelp ainsi que son style

---
+++ needhelp.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+require_once("templates/header.php");
+?>
+
+<div class="main-needhelp">
+    <h1>Cor-sell Assistance</h1>
+    <div class="p-main-needhelp">
+        <p> Vous avez rencontré un problème avec notre site? Vous avez besoin d'aide quelqu'elle soit?</p>
+        <p class="corsell-strong-erre"> Cor-sell à tout prévue!</p>
+        <p> Pour cela venez nous voir à la taverne du chat bogne et demander l 'unité spécial D.É.D.É</p>
+        <p>L'unité D.É.D.É (Dinausore, Énergique, Dynamique ,Éblouissant ) est la tout les jour de 8H à 22H  </p>
+    </div>
+    <div class="div-img-needhelp">
+        <img src="./assets/img/dede.jpg" alt="" class="img-needhelp">
+    </div>
+    <div class="p-main-needhelp">
+        <p class="corsell-strong-erre">
+            Cor-sell Assistance </p>
+        <p>
+            Si vous ne voyez pas dédé annoncer une tourné général et il arrivera plus vite que l'éclaire.</p>
+        <p class="corsell-strong-erre">Cor-sell Assistance </p>
+       
+        <p> Si dédé ne vous repond pas, parler juste plus fort.
+        </p>
+        <p class="corsell-strong-erre">Cor-sell Assistance </p>
+        <p> Les réponses de dédé sont garantie 100% juste*
+        </p>
+        <p class="corsell-strong-erre">Cor-sell Assistance </p>
+        <p> Si dédé vous crie dessus c'est pas qu'il vous aime pas c 'est juste qu'il est un peu dur de la feuille...
+        </p>
+    </div>
+    <div class="little-needhelp">
+    <p>*les reponses ne sont pas garanties juste c 'est jsute qu'il nous à forcé à le dire.</p>
+    </div>
+
+
+
+
+</div>
+
+<?php
+require_once("templates/footer.php");
+?>
