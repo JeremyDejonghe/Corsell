64035bec696772dfe92b3a0cb70c818f8cf9482a
Jérémy Dejonghe <jeremy.dejonghe59@gmail.com>
2021-04-22 14:13:11 +0200

    Correction lien favicon local

--- snap-ship.php
+++ snap-ship.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/main.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link rel="icon" href="/assets/img/favicon-corsell.ico" type="image/x-icon">
+    <link rel="icon" href="./assets/img/favicon-corsell.ico" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css2?family=Spectral:ital,wght@0,400;0,600;0,800;1,400;1,600;1,800&display=swap" rel="stylesheet">
     <title>Snap-Ship</title>
 </head>
